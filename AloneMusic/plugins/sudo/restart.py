b9d5e31ba1a63d616f58d5968dbb0670e19c4e59
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 04:42:04 +0530

    Update restart.py

--- AloneMusic/plugins/sudo/restart.py
+++ AloneMusic/plugins/sudo/restart.py
@@ -10,15 +10,15 @@ from git.exc import GitCommandError, InvalidGitRepositoryError
 from pyrogram import filters
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import HAPP, SUDOERS, XCB
-from AnonXMusic.utils.database import (
+from AloneMusic import app
+from AloneMusic.misc import HAPP, SUDOERS, XCB
+from AloneMusic.utils.database import (
     get_active_chats,
     remove_active_chat,
     remove_active_video_chat,
 )
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.pastebin import AnonyBin
+from AloneMusic.utils.decorators.language import language
+from AloneMusic.utils.pastebin import AnonyBin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
