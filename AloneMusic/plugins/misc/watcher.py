a581901c51e7c0daa586b4ee8e7986217d8d96da
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 04:04:02 +0530

    𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ

--- AloneMusic/plugins/misc/watcher.py
+++ AloneMusic/plugins/misc/watcher.py
@@ -1,8 +1,17 @@
+#
+# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
+#
+# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
 from pyrogram import filters
 from pyrogram.types import Message
 
 from AloneMusic import app
-from AloneMusic.core.call import Anony
+from AloneMusic.core.call import Alone
 from AloneMusic.utils.database import get_assistant
 
 welcome = 20
