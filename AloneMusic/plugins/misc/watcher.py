74d5cfcf21a141cee7dd600770a22791ac0ae85e
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 04:03:33 +0530

    Update watcher.py

--- AloneMusic/plugins/misc/watcher.py
+++ AloneMusic/plugins/misc/watcher.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import get_assistant
+from AloneMusic import app
+from AloneMusic.core.call import Anony
+from AloneMusic.utils.database import get_assistant
 
 welcome = 20
 close = 30
@@ -12,7 +12,7 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await Anony.stop_stream_force(message.chat.id)
+    await Alone.stop_stream_force(message.chat.id)
 
 @app.on_message(filters.left_chat_member, group=69)
 async def bot_kick(_, msg: Message):
