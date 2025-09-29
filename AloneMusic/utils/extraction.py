505efd9cbfa62ca470aec479f40ab9a8b2c913d7
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 05:03:41 +0530

    𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ

--- AloneMusic/utils/extraction.py
+++ AloneMusic/utils/extraction.py
@@ -1,7 +1,16 @@
+#
+# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
+#
+# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from AnonXMusic import app
+from AloneMusic import app
 
 
 async def extract_user(m: Message) -> User:
