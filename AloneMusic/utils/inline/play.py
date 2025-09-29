855ece1551f928be5e1bd1473b58f1c06a01f510
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 05:22:46 +0530

    𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ

--- AloneMusic/utils/inline/play.py
+++ AloneMusic/utils/inline/play.py
@@ -1,8 +1,17 @@
+#
+# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
+#
+# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
 import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from AnonXMusic.utils.formatters import time_to_seconds
+from AloneMusic.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
