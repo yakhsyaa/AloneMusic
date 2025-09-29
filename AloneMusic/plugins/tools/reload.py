1f8b8993f97a7695b928b412c3a0acafd83b0176
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 04:52:53 +0530

    Update reload.py

--- AloneMusic/plugins/tools/reload.py
+++ AloneMusic/plugins/tools/reload.py
@@ -5,12 +5,12 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import CallbackQuery, Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import get_assistant, get_authuser_names, get_cmode
-from AnonXMusic.utils.decorators import ActualAdminCB, AdminActual, language
-from AnonXMusic.utils.formatters import alpha_to_int, get_readable_time
+from AloneMusic import app
+from AloneMusic.core.call import Anony
+from AloneMusic.misc import db
+from AloneMusic.utils.database import get_assistant, get_authuser_names, get_cmode
+from AloneMusic.utils.decorators import ActualAdminCB, AdminActual, language
+from AloneMusic.utils.formatters import alpha_to_int, get_readable_time
 from config import BANNED_USERS, adminlist, lyrical
 
 rel = {}
