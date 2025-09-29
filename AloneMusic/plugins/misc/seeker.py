62bf88fe103a001b93f9106255535bbf8093071c
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 04:02:24 +0530

    Update seeker.py

--- AloneMusic/plugins/misc/seeker.py
+++ AloneMusic/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import get_active_chats, is_music_playing
+from AloneMusic.misc import db
+from AloneMusic.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
