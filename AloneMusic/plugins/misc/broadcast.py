b6e1cfdacda0ff6f08c48b35dcc7cb3805bf1493
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 04:01:17 +0530

    Update broadcast.py

--- AloneMusic/plugins/misc/broadcast.py
+++ AloneMusic/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from AloneMusic import app
+from AloneMusic.misc import SUDOERS
+from AloneMusic.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.formatters import alpha_to_int
+from AloneMusic.utils.decorators.language import language
+from AloneMusic.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
