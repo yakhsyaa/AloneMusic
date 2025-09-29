3c1008629dbd7864fb2467ded40e0e8cfa6faac4
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 03:35:41 +0530

    Update seek.py

--- AloneMusic/plugins/admins/seek.py
+++ AloneMusic/plugins/admins/seek.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AdminRightsCheck, seconds_to_min
-from AnonXMusic.utils.inline import close_markup
+from AloneMusic import YouTube, app
+from AloneMusic.core.call import Alone
+from AloneMusic.misc import db
+from AloneMusic.utils import AdminRightsCheck, seconds_to_min
+from AloneMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
