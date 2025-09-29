161f71c2edbe6c53dbc649ca6f0b105962ffc2f7
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 05:04:52 +0530

    Update database.py

--- AloneMusic/utils/database.py
+++ AloneMusic/utils/database.py
@@ -3,8 +3,8 @@ import asyncio
 from datetime import date
 from typing import Dict, List, Union
 
-from AnonXMusic import userbot
-from AnonXMusic.core.mongo import mongodb
+from AloneMusic import userbot
+from AloneMusic.core.mongo import mongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
@@ -71,7 +71,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from AnonXMusic.core.userbot import assistants
+    from AloneMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -85,7 +85,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from AnonXMusic.core.userbot import assistants
+    from AloneMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -112,7 +112,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from AnonXMusic.core.userbot import assistants
+    from AloneMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -125,7 +125,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from AnonXMusic.core.userbot import assistants
+    from AloneMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
