d9d44c0f5e23aa59575dd0e882be091afa649040
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 04:49:29 +0530

    Update queue.py

--- AloneMusic/plugins/tools/queue.py
+++ AloneMusic/plugins/tools/queue.py
@@ -6,12 +6,12 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AnonyBin, get_channeplayCB, seconds_to_min
-from AnonXMusic.utils.database import get_cmode, is_active_chat, is_music_playing
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline import queue_back_markup, queue_markup
+from AloneMusic import app
+from AloneMusic.misc import db
+from AloneMusic.utils import AloneBin, get_channeplayCB, seconds_to_min
+from AloneMusic.utils.database import get_cmode, is_active_chat, is_music_playing
+from AloneMusic.utils.decorators.language import language, languageCB
+from AloneMusic.utils.inline import queue_back_markup, queue_markup
 from config import BANNED_USERS
 
 basic = {}
