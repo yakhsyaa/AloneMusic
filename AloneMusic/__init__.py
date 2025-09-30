38197dd5c844519eda90f1589678379cf741a8ac
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 05:33:09 +0530

    𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ

--- AloneMusic/__init__.py
+++ AloneMusic/__init__.py
@@ -1,7 +1,16 @@
-from AnonXMusic.core.bot import Anony
-from AnonXMusic.core.dir import dirr
-from AnonXMusic.core.userbot import Userbot
-from AnonXMusic.misc import dbb, heroku
+#
+# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
+#
+# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from AloneMusic.core.bot import Alone
+from AloneMusic.core.dir import dirr
+from AloneMusic.core.userbot import Userbot
+from AloneMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
@@ -9,7 +18,7 @@ dirr()
 dbb()
 heroku()
 
-app = Anony()
+app = Alone()
 userbot = Userbot()
 
 
