7bf7fda0a53195bfbc00f27d33e5850dcccb0bbc
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 06:23:59 +0530

    Update help.py

--- AloneMusic/plugins/bot/help.py
+++ AloneMusic/plugins/bot/help.py
@@ -19,6 +19,7 @@ from AloneMusic.utils.decorators.language import LanguageStart, languageCB
 from AloneMusic.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
+from AloneMusic.misc import SUDOERS
 
 
 @app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
