1c42d7753caf5e9ac6171ed9645b44f3b899b451
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 08:27:17 +0530

    𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ

--- AloneMusic/plugins/sudo/restart.py
+++ AloneMusic/plugins/sudo/restart.py
@@ -4,7 +4,6 @@
 # This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
 # All rights reserved.
 
 import asyncio
