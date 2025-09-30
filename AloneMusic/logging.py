6debe3f87b1b7c474ef16891a46127179efb183b
𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ <riteshcoderx@gmail.com>
2025-09-30 08:25:11 +0530

    𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ

--- AloneMusic/logging.py
+++ AloneMusic/logging.py
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
 # This file is part of < https://github.com/TheAloneTeam/TheAloneMusic > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/TheAloneTeam/TheAloneMusic/blob/master/LICENSE >
